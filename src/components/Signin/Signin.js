005b0e0fcca903e6dc6285841095271c9ae06f08
hemis23 <themischa9@gmail.com>
2024-02-26 11:10:22 +0200

    link to server

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -15,7 +15,7 @@ class  Signin extends React.Component{
         this.setState({signInPassword: event.target.value})
     }
     onSubmitSignin = () =>{
-        fetch("http://localhost:3000/signin",{
+        fetch("https://smart-brain-api-329v.onrender.com/signin",{
             method:"post",
             headers:{"Content-Type" : "application/json"},
             body: JSON.stringify({
